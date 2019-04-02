c594085c94024cc802ed9bc610bc784abbbfa02a
Hyungsuk Kang <hskang9@gmail.com>
2019-04-03 05:33:15 +0900

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 const IconLoader = () => (
-  <svg id="logo" width="47px" height="28px" viewBox="0 0 47 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <title>Loader Logo</title>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-family="AppleSDGothicNeo-Bold, Apple SD Gothic Neo" font-size="39" font-weight="bold">
-        <g id="Group" transform="translate(-26.000000, -37.000000)" fill="#00FFD8">
-            <g id="B">
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="B" transform="translate(-26.000000, -37.000000)" fill="#64FFDA">
+            <g id="LM">
                 <text>
                     <tspan x="23.285" y="66">LM</tspan>
                 </text>
             </g>
         </g>
+      <path
+        stroke="#64FFDA"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
     </g>
-</svg>
+  </svg>
 );
 
 export default IconLoader;
