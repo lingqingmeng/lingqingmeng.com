0698ae49b324e6a7e0c65ef7d8bd2e735d558258
Hyungsuk Kang <hskang9@gmail.com>
2019-04-03 05:53:51 +0900

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,13 +4,6 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="B" transform="translate(11.000000, 5.000000)" fill="#64FFDA">
-            <g id="LM">
-                <text>
-                    <tspan x="100" y="100">LM</tspan>
-                </text>
-            </g>
-        </g>
       <path
         stroke="#64FFDA"
         strokeWidth="5"
@@ -23,6 +16,13 @@ const IconLoader = () => (
                   L 89, 73
                   L 89, 28 z"
       />
+     <g id="B" transform="translate(11.000000, 5.000000)" fill="#64FFDA">
+            <g id="LM">
+                <text id="LM" fill="#64FFDA" font-family="AppleSDGothicNeo-Bold, Apple SD Gothic Neo" font-size="39" font-weight="bold">
+                    <tspan x="13" y="58">LM</tspan>
+                </text>
+            </g>
+        </g>
     </g>
   </svg>
 );
