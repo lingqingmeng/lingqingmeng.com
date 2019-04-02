fb1de0c7f9d77e7b7032767faa9571d2c2fc6b2e
Hyungsuk Kang <hskang9@gmail.com>
2019-04-03 05:32:03 +0900

    Update loader.js

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,30 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+import anime from 'animejs';
+import { IconLoader } from './iLocons';
+import styled from 'styled-components';
+import { theme, mixins } from '../styles';
+const { colors } = theme;
 
-const IconLoader = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <title>Loader Logo</title>
-    <g>
-      <g id="B" transform="translate(-26.000000, -37.000000)" fill="#64FFDA">
-            <g id="LM">
-                <text>
-                    <tspan x="23.285" y="66">LM</tspan>
-                </text>
-            </g>
-        </g>
-      <path
-        stroke="#64FFDA"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
-    </g>
-  </svg>
-);
+const LoaderContainer = styled.div`
+  ${mixins.flexCenter};
+  background-color: ${colors.darkNavy};
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+`;
+const LogoWrapper = styled.div`
+  width: max-content;
+  max-width: 100px;
+  transition: ${theme.transition};
+  opacity: ${props => (props.isMounted ? 1 : 0)};
+  svg {
+    width: 100%;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+    fill: none;
+    user-select: none;
+    #B {
+      opacity: 0;
+    }
+  }
+`;
 
-export default IconLoader;
+class Loader extends Component {
+  static propTypes = {
+    finishLoading: PropTypes.func.isRequired,
+  };
+
+  state = {
+    isMounted: false,
+  };
+
+  componentDidMount() {
+    this.setState({ isMounted: true }, () => this.animate());
+  }
+
+  componentWillUnmount() {
+    this.setState({ isMounted: false });
+  }
+
+  animate() {
+    const { finishLoading } = this.props;
+
+    const loader = anime.timeline({
+      complete: () => finishLoading(),
+    });
+
+    loader
+      .add({
+        targets: '#logo path',
+        delay: 500,
+        duration: 2000,
+        easing: 'easeInOutQuart',
+        strokeDashoffset: [anime.setDashoffset, 0],
+      })
+      .add({
+        targets: '#logo #B',
+        duration: 800,
+        easing: 'easeInOutQuart',
+        opacity: 1,
+      })
+      .add({
+        targets: '#logo',
+        delay: 700,
+        duration: 300,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        scale: 0.1,
+      })
+      .add({
+        targets: '.loader',
+        duration: 200,
+        easing: 'easeInOutQuart',
+        opacity: 0,
+        zIndex: -1,
+      });
+  }
+
+  render() {
+    const { isMounted } = this.state;
+
+    return (
+      <LoaderContainer className="loader">
+        <Helmet>
+          <body className={isMounted ? 'hidden' : ''} />
+        </Helmet>
+        <LogoWrapper isMounted={isMounted}>
+          <IconLoader />
+        </LogoWrapper>
+      </LoaderContainer>
+    );
+  }
+}
+
+export default Loader;
