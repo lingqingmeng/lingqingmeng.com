45f40bad20dd96b085fc29d047d0c39c6b1ae584
cco <cco@lingqingmeng.com>
2023-02-11 15:05:52 -0500

    save state

--- src/components/userform.js
+++ src/components/userform.js
@@ -6,8 +6,9 @@ import styled from 'styled-components';
 import { theme, mixins, media, Section, Heading, Button } from '../styles';
 
 const UserFormContainer = styled.main`
-  position: absolute;
+  position: relative;
 `;
+
 const FlexContainer = styled.div`
   ${mixins.flexBetween};
   align-items: flex-start;
@@ -29,92 +30,83 @@ const Title = styled.h1`
   ${media.tablet`font-size: 40px;`};
 `;
 
-const SendButton = styled(Button)`
+const SendButton = styled(Button)``;
 
-`;
-
-
-
-class InformationForm {
-
-};
+class InformationForm {}
 
 class UserForm extends Component {
-    static propTypes = {
-      data: PropTypes.array.isRequired,
-    };
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+  };
 
-    constructor(props) {
-        super(props);
-      }
-
-    render() {
-      const { data } = this.props;
-      const { frontmatter, html } = data[0].node;
-      const { title, firstname, lastname, email, company, industry } = frontmatter;
-  
-      return (
-        <UserFormContainer id="userform" ref={el => (this.userform = el)}>
-          <Title>{title}</Title>
-
-            <form method="post" action="#TODO">
-
-            <div>
-              <label>
-              {firstname}
-                <div>
-                  <input type="text" name="name" id="firstname" />
-                </div>
-              </label>
-            </div>
-
-            <div>
-                <label>
-                {lastname}
-                  <div>
-                    <input type="text" name="name" id="lastname" />
-                  </div>
-                </label>
-              </div>
-
-              <div>
-                <label>
-                  {company}
-                  <div>
-                    <input type="text" name="subject" id="subject" />
-                  </div>
-                </label>
-              </div>              
-
-              <div>
-                <label>
-                  {email}
-                  <div>
-                    <input type="email" name="email" id="emailaddress" />
-                  </div>
-                </label>
-              </div>
-
-              <div>
-                <label>
-                  {industry}
-                  <div>
-                    <input type="text" name="subject" id="subject" />
-                  </div>
-
-                </label>
-              </div>
-
-              <div>
-              <SendButton>
-                <button type="submit">Access our One-Pager!</button>
-              </SendButton>
-              </div>
-
-          </form>
-        </UserFormContainer>
-      );
-    }
+  constructor(props) {
+    super(props);
   }
-  
-  export default UserForm;
+
+  render() {
+    const { data } = this.props;
+    const { frontmatter, html } = data[0].node;
+    const { title, firstname, lastname, email, company, industry } = frontmatter;
+
+    return (
+      <UserFormContainer id="userform" ref={el => (this.userform = el)}>
+        <Title>{title}</Title>
+
+        <form method="post" action="#TODO">
+          <div>
+            <label>
+              {firstname}
+              <div>
+                <input type="text" name="name" id="firstname" />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              {lastname}
+              <div>
+                <input type="text" name="name" id="lastname" />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              {company}
+              <div>
+                <input type="text" name="subject" id="subject" />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              {email}
+              <div>
+                <input type="email" name="email" id="emailaddress" />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <label>
+              {industry}
+              <div>
+                <input type="text" name="subject" id="subject" />
+              </div>
+            </label>
+          </div>
+
+          <div>
+            <SendButton>
+              <button type="submit">Access our One-Pager!</button>
+            </SendButton>
+          </div>
+        </form>
+      </UserFormContainer>
+    );
+  }
+}
+
+export default UserForm;
--- src/pages/MarketingUserForm.js
+++ src/pages/MarketingUserForm.js
@@ -1,22 +1,31 @@
-import * as React from 'react'
+import * as React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import UserForm from '../components/userform';
+import styled from 'styled-components';
+import Layout from '../components/layout';
+import { mixins, Main } from '../styles';
+const MainContainer = styled(Main)`
+  ${mixins.sidePadding};
+  counter-reset: section;
+`;
 
-const MarketingUserForm = ({ data}) => {
+const MarketingUserForm = ({ data }) => {
   return (
-    <UserForm data={data.userform.edges} />
-  )
+    <Layout location={location}>
+      <MainContainer id="content">
+        <UserForm data={data.userform.edges} />
+      </MainContainer>
+    </Layout>
+  );
 };
 
 MarketingUserForm.propTypes = {
-    data: PropTypes.object.isRequired,
-    location: PropTypes.object,
-  };
-
+  data: PropTypes.object.isRequired,
+  location: PropTypes.object,
+};
 
 export default MarketingUserForm;
-
 
 export const query = graphql`
   query aQuery {
