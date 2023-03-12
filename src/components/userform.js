772f8f0ebc2b0c28586c9214bf88b2218acfeda8
German Garcia <german@ondecentral.com>
2023-03-12 15:44:15 -0400

    Changed mixpanel logging to use production API

--- src/components/userform.js
+++ src/components/userform.js
@@ -87,7 +87,7 @@ class UserForm extends Component {
       source: 'Decentral Portal',
     };
     try {
-      fetch('https://api.stage.founderstable.xyz' + '/mixpanel', {
+      fetch('https://api.founderskit.org' + '/mixpanel', {
         //fetch('http://localhost:3000' + '/mixpanel', {
         method: 'POST',
         headers: {
